34f83119ecbf1b8b5d44b247c6cbe1d64829fdd9
RasamizafyB <rasamizafybryan98@gmail.com>
2020-06-19 13:37:11 +0200

    add animation arena

--- src/js/gamemanager.js
+++ src/js/gamemanager.js
@@ -101,11 +101,6 @@ function GameManager() {
         switch (racePlayer1) {
             case 'orc':
                 player1 = new orc(itemPlayer1);
-                document.querySelector('img') = '';
-                let img = document.createElement('img')
-                img.src = 'src/image/orc.jpg'
-                var block = document.getElementById("source");
-                block.appendChild(img);
                 break;
 
             case 'humain':
@@ -171,16 +166,12 @@ function GameManager() {
 
         // player2.heal();
         
-        document.querySelector('.hit1').addEventListener('click', function(){
             logs(player1, player2);
             player1attack()
-        });
 
 
-        document.querySelector('.hit2').addEventListener('click', function(){
             logs(player2, player1);
             player2attack()
-        });
 
         // updateLifeBar();
 
